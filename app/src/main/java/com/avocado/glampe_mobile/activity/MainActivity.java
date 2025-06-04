18ef08e642f37dd60d972954df4d861e8bf0eda9
TanThinh <thinhhtse183224@fpr.edu.vn>
2025-06-04 23:57:52 +0700

    update layout profile

--- app/src/main/java/com/avocado/glampe_mobile/activity/MainActivity.java
+++ app/src/main/java/com/avocado/glampe_mobile/activity/MainActivity.java
@@ -4,13 +4,27 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavController;
+import androidx.navigation.fragment.NavHostFragment;
+import androidx.navigation.ui.NavigationUI;
 
 import com.avocado.glampe_mobile.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        NavHostFragment navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
+
+        if (navHostFragment != null) {
+            NavController navController = navHostFragment.getNavController();
+
+            BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+            NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        } else throw new IllegalStateException("NavHostFragment not found");
     }
 }
--- app/src/main/java/com/avocado/glampe_mobile/fragment/ProfileFragment.java
+++ app/src/main/java/com/avocado/glampe_mobile/fragment/ProfileFragment.java
@@ -1,11 +1,25 @@
 package com.avocado.glampe_mobile.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.avocado.glampe_mobile.R;
 
 public class ProfileFragment extends Fragment {
-    public ProfileFragment(){
-        super(R.layout.fragment_profile);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_profile, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
     }
 }
